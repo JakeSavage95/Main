57951aa2f1e2608a73b9dfc77478e51ebce23904
SnowblindFox <jakesavage@live.co.uk>
2021-11-08 14:47:40 +0000

    Code Samples
    Excerpts from personal projects to demonstrate coding practices

---
+++ GestureInputSample.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+using XR.General;
+using VarjoInput;
+
+/// <summary>
+/// A generic gesture input system that draws a many to one link between VR APKS and globally accessible bools for multi platform support
+/// </summary>
+public class GestureInputSample : MonoBehaviour
+{
+    public static GestureInputSample Instance;
+    #region Unity Editor Variables
+    [Header("SteamVRControllers")]
+    [SerializeField] Hand _leftSteamController = null;
+    [SerializeField] Hand _rightSteamController = null;
+
+    [Header("OculusVRControllers")]
+    [SerializeField] Transform _leftOculusController = null;
+    [SerializeField] Transform _rightOculusController = null;
+
+    [Header("VarjoRControllers")]
+    [SerializeField] Controller _rightVarjoController = null;
+    [SerializeField] Controller _leftVarjoController = null;
+    #endregion// Unity Editor Variables
+
+    #region Private Variables
+    Transform _rightControllerTransform = null;
+    Transform _leftcontrollerTransform = null;
+
+    bool _rightTriggerPressed;
+    bool _leftTriggerPressed;
+    bool _rightTouchPressed;
+    bool _leftTouchPressed;
+    bool _rightMenuButtonPressed;
+    bool _leftMenuButtonPressed;
+    bool _rightGripPressed;
+    bool _leftGripPressed;
+
+    Vector2 _rightTouchPosition;
+    Vector2 _leftTouchPosition;
+    #endregion //Private Variables
+
+    #region Public Properties
+    //This area contains all public input, allowing platform agnostic input to be called form elsewhere in the app
+    public bool RightTriggerPressed { get { return _rightTriggerPressed; } private set { } }
+    public bool LeftTriggerPressed { get { return _leftTriggerPressed; } private set { } }
+    public bool RightTouchPressed { get { return _rightTouchPressed; } private set { } }
+    public bool LeftTouchPressed { get { return _leftTouchPressed; } private set { } }
+    public bool RightMenuButtonPressed { get { return _rightMenuButtonPressed; } private set { } }
+    public bool LeftMenuButtonPressed { get { return _leftMenuButtonPressed; } private set { } }
+    public bool RightGripPressed { get { return _rightGripPressed; } private set { } }
+    public bool LeftGripPressed { get { return _leftGripPressed; } private set { } }
+
+    public Vector2 RightTouchPosition { get { return _rightTouchPosition; } private set { } }
+    public Vector2 LeftTouchPosition { get { return _leftTouchPosition; } private set { } }
+    #endregion // Public Properties
+
+    #region Private Unity Methods
+    //Intialise static instance and controller transforms Using a seperate manager to determine platfrom
+    void Start()
+    {
+        _XRRaycaster = XRRaycaster.Instance;
+        switch(XRManager.Instance.ThisVRPlatform)
+        {
+            case VRPlatform.SteamVR:
+                _rightControllerTransform = _rightSteamController.transform;
+                _leftcontrollerTransform = _leftSteamController.transform;
+                break;
+            case VRPlatform.Oculus:
+                _rightControllerTransform = _rightOculusController;
+                _leftcontrollerTransform = _leftOculusController;
+                break;
+            case VRPlatform.Varjo:
+                _rightControllerTransform = _rightVarjoController.transform;
+                _leftcontrollerTransform = _leftVarjoController.transform;
+                break;
+
+        }
+        _rightControllerTransform = _rightVarjoController.transform;
+        _leftcontrollerTransform = _leftVarjoController.transform;
+    }
+
+    // Using the XRManager, determine current platform and poll for Input from the appropriate SDK
+    void Update()
+    {
+        _rightTouchPosition = Vector2.zero;
+        _leftTouchPosition = Vector2.zero;
+
+        if(XRManager.Instance.ThisVRPlatform == VRPlatform.SteamVR)
+        {
+            _rightTriggerPressed = SteamVR_Actions.TheoremVRActions.RightTrigger.GetStateDown(SteamVR_Input_Sources.Any);
+            _leftTriggerPressed = SteamVR_Actions.TheoremVRActions.LeftTrigger.GetStateDown(SteamVR_Input_Sources.Any);
+
+            _rightGripPressed = SteamVR_Actions.TheoremVRActions.RightGrip.GetStateDown(SteamVR_Input_Sources.Any);
+            _leftGripPressed = SteamVR_Actions.TheoremVRActions.LeftGrip.GetStateDown(SteamVR_Input_Sources.Any);
+
+            _rightTouchPressed = SteamVR_Actions.TheoremVRActions.RightDirectionalPress.GetStateDown(SteamVR_Input_Sources.Any);
+            _leftTouchPressed = SteamVR_Actions.TheoremVRActions.LeftDirectionalPress.GetStateDown(SteamVR_Input_Sources.Any);
+
+            _rightMenuButtonPressed = SteamVR_Actions.TheoremVRActions.RightMenubutton.GetStateDown(SteamVR_Input_Sources.Any);
+            _leftMenuButtonPressed = SteamVR_Actions.TheoremVRActions.LeftMenuButton.GetStateDown(SteamVR_Input_Sources.Any);
+
+            _rightTouchPosition = SteamVR_Actions.TheoremVRActions.RightDirectional.GetAxis(SteamVR_Input_Sources.Any);
+            _leftTouchPosition = SteamVR_Actions.TheoremVRActions.LeftDirectional.GetAxis(SteamVR_Input_Sources.Any);
+        }
+        else if(XRManager.Instance.ThisPlatform == VRPlatform.Oculus)
+        {
+            _rightTriggerPressed = OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger);
+            _leftTriggerPressed = OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger);
+
+            _rightGripPressed = OVRInput.GetDown(OVRInput.RawButton.RHandTrigger);
+            _leftGripPressed = OVRInput.GetDown(OVRInput.RawButton.LHandTrigger);
+
+            _rightTouchPressed = OVRInput.GetDown(OVRInput.RawButton.RThumbstick);
+            _leftTouchPressed = OVRInput.GetDown(OVRInput.RawButton.LThumbstick);
+
+            _rightMenuButtonPressed = OVRInput.GetDown(OVRInput.RawButton.A);
+            _leftMenuButtonPressed = OVRInput.GetDown(OVRInput.RawButton.X);
+
+            _rightTouchPosition = OVRInput.Get(OVRInput.RawAxis2D.RThumbstick);
+            _leftTouchPosition = OVRInput.Get(OVRInput.RawAxis2D.LThumbstick);
+        }
+        if (XRManager.Instance.ThisPlatform == VRPlatform.Varjo)
+        {
+
+            _rightTriggerPressed = _rightVarjoController.TriggerButtonDown;
+            _leftTriggerPressed = _leftVarjoController.TriggerButtonDown;
+
+            _rightGripPressed = _rightVarjoController.GripButtonDown;
+            _leftGripPressed = _leftVarjoController.GripButtonDown;
+
+            _rightTouchPressed = _rightVarjoController.TouchpadClickDown;
+            _leftTouchPressed = _leftVarjoController.TouchpadClickDown;
+
+            _rightMenuButtonPressed = _rightVarjoController.MenuButtonDown;
+            _leftMenuButtonPressed = _leftVarjoController.MenuButtonDown;
+
+            _rightTouchPosition = _rightVarjoController.TouchpadTouchPosition;
+            _leftTouchPosition = _leftVarjoController.TouchpadTouchPosition;
+        }
+    }
+    #endregion// Private Unity Methods
+}
---
+++ PanoramicMediaPackageReader.cs
@@ -0,0 +1,326 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System;
+using System.Text.RegularExpressions;
+using TMPro;
+using UnityEngine.Networking;
+using XR.External;
+using XR.General;
+using UnityEngine.Video;
+
+/// <summary>
+/// This class reads a CSV of panoramic media and data relating to the media and uses it to create A series of Points that can be used to view the Media Immersivley
+/// </summary>
+public class PanoramicZipPackageReader : MonoBehaviour
+{
+    public static PanoramicZipPackageReader Instance;
+
+    #region Unity Editor Variables
+    [SerializeField] GameObject _panoramicSpherePrefab = null;
+    #endregion
+
+    #region Private Variables
+    Transform _cameraRig = null;
+    const char LINE_SEPERATOR = '\n';
+    const char VALUE_SEPERATOR = ' ';
+
+    bool _cancelFileParse = false;
+    string licenseError = "";
+    List<Point> _convertedPoints = null;
+    List<PanoramicSphere> _panoramicPoints = null;
+    List<PanoramicMediaSphereBehaviour> _panoramicMediaSphereBehaviours = null;
+    List<Material> _materials = null;
+    List<string> _imageNames = null;
+    #endregion //Private Variables
+
+    #region Public Properties
+    public Texture2D CurrentTexture { get; set; }
+    public GameObject CurrentSphere { get; set; }
+    public Transform CameraRig { get { return _cameraRig; } }
+    #endregion //Public Properties
+
+    #region Public Methods
+    public void StartFileRead(string fullFilePath)
+	{
+		Debug.LogWarning("FullPath = " + fullFilePath);
+		ExtractImageFolder(fullFilePath);
+	}
+
+	public void FileParse(string fullPath)
+	{
+        Debug.Log("Path = " + fullPath);
+		string folderPath = Path.GetDirectoryName(fullPath);
+
+		LinesLoaded = true;
+
+		StreamReader streamReader = new StreamReader(new FileStream(fullPath, FileMode.Open, FileAccess.Read, FileShare.Read));
+		string line;
+
+		streamReader.ReadLine(); // skip first line
+		int i = 1;
+        PanoramicSphere.MediaType type = PanoramicSphere.MediaType.Image;
+        while (!_cancelFileParse && (line = streamReader.ReadLine()) != null)
+		{
+			KeyValuePair<Vector3, string> nameAndPos;
+			string[] values = line.Split('\t', ',');
+            PanoramicSphere temporaryPointClass = new PanoramicSphere();
+            if (values.Length == 9 && values[8].Length > 0)
+            {
+				try
+				{
+                    switch(values[1])
+                    {
+                        case "IMAGE":
+                            type = PanoramicSphere.MediaType.Image;
+                            break;
+                        case "VIDEO":
+                            type = PanoramicSphere.MediaType.Video;
+                            break;
+                    }
+
+                    float x = 0;
+                    float y = 0;
+
+                    if (float.TryParse(values[2],out x) && float.TryParse(values[3],out y))
+					{
+						Vector3 position = new Vector3(x,0,y);
+                        string imagePath = values[8].Replace("files/", "");
+                        imagePath = imagePath.Replace("/", "\\");
+                        string mediaFolderName = "Images";
+                        string mediaFolderPath = imagePath; // Path.Combine(mediaFolderName, imagePath);
+                        string fullMediaPath = Path.Combine(folderPath, mediaFolderPath);
+						Debug.Log("Media file path = " + fullMediaPath);
+						if (File.Exists(Path.Combine(folderPath, mediaFolderPath)))
+						{
+							if (values[1] == "") // No Name Found
+							{
+								nameAndPos = new KeyValuePair<Vector3, string>(position, string.Format("No Media name on Line #{0}", i.ToString()));
+							}
+							else
+								nameAndPos = new KeyValuePair<Vector3, string>(position, values[1]);
+
+                            temporaryPointClass.FileName = fullMediaPath;
+						}
+						else
+						{
+							Debug.LogFormat("Couldn't find Media #{0}", i.ToString());
+							nameAndPos = new KeyValuePair<Vector3, string>(position, string.Format("Couldn't find Media #{0}", i.ToString()));
+                            temporaryPointClass.FileName = string.Format("Line {0} is erroneous. Could Not Find File", i.ToString());
+                        }
+                    }
+					else //Position failed to parse
+					{
+						Debug.LogFormat("Could not read position #{0}", i.ToString());
+						Vector3 position = new Vector3(i, i, i);
+						nameAndPos = new KeyValuePair<Vector3, string>(position, string.Format("Could not read position #{0}", i.ToString()));
+                        temporaryPointClass.FileName = string.Format("Line {0} is erroneous. Position Could not be read", i.ToString());
+                    }
+
+
+                }
+				catch
+				{
+					Debug.LogFormat("Could not read line #{0} Please check format", i.ToString());
+					Vector3 position = new Vector3(i, i, i);
+					nameAndPos = new KeyValuePair<Vector3, string>(position, string.Format("Line {0} is erroneous.", i.ToString()));
+                    temporaryPointClass.FileName = string.Format("Line {0} is erroneous.", i.ToString());
+                }
+            }
+			else
+			{
+				Debug.LogFormat("Incorrect entry count in Line #{0}.", i.ToString());
+				Vector3 position = new Vector3(i, i, i);
+				nameAndPos = new KeyValuePair<Vector3, string>(position, string.Format("Incorrect entry count in Line #{0}.", i.ToString()));
+                temporaryPointClass.FileName = string.Format("Line {0} is erroneous. Incorrect Line Length Too Many Entries", i.ToString());
+            }
+            temporaryPointClass.Position = nameAndPos.Key;
+            temporaryPointClass.Name = nameAndPos.Value;
+            temporaryPointClass.Type = type;
+            _panoramicPoints.Add(temporaryPointClass);
+            i++;
+		}
+
+        streamReader.Dispose();
+		if (_cancelFileParse)
+		{
+			Debug.LogWarning("FileParse while loop was cancelled");
+		}
+		CreateSpheres();
+	}
+
+	public void ChangeSkybox (int value)
+	{
+		_panoramicMediaSphereBehaviours[value].DropDownValuechanged();
+	}
+    #endregion //Public Methods
+
+    #region Private Methods
+    /// <summary>
+    /// Extract a media package from the designated zip path. Begin reading the CSV
+    /// </summary>
+    /// <param name="path"></param>
+    void ExtractImageFolder(string path)
+	{
+		TemporaryFileManager _cacheManager = XRUtilities.Get<TemporaryFileManager>();
+		ZipFile zip = new ZipFile(path);
+
+		string mediaPackageName = Path.GetFileNameWithoutExtension(path);
+		string folderPath = Path.Combine(_cacheManager.CachePath, mediaPackageName);
+        zip.ExtractExistingFile = ExtractExistingFileAction.OverwriteSilently;
+		zip.ExtractAll(folderPath);
+        zip.Dispose();
+		string fullFilePath = "";
+
+		string[] files = Directory.GetFiles(folderPath, "*.csv", SearchOption.AllDirectories);
+		if (files != null && files.Length > 0)
+			fullFilePath = files[0];
+		else
+			Debug.LogError("Unable to locate file matching pattern '*.csv' in folder: " + folderPath);
+
+		if (File.Exists(fullFilePath))
+			FileParse(fullFilePath);
+		else
+			Debug.LogError("No File exists at path: " + fullFilePath);
+	}
+
+    /// <summary>
+    /// Parse all MediaSphere data from the extracted CSV. Generate Panoramic media spheres and then compile a list of all available spheres.
+    /// Lastly create Teleportable points for 2D UI interaction.
+    /// </summary>
+	void CreateSpheres()
+	{
+		int index = 0;
+		bool firstImageFound = true;
+		PanoramicMediaSphereBehaviour firstCorrectPath = null;
+
+        foreach (PanoramicSphere panoramicPoint in _panoramicPoints)
+        {
+            string mediaName = panoramicPoint.Name;
+            string mediaPath = panoramicPoint.FileName;
+
+            Transform sphere = GameObject.Instantiate(_panoramicSpherePrefab).transform;
+            sphere.position = panoramicPoint.Position;
+
+            PanoramicMediaSphereBehaviour _panoramicMediaSphereBehaviour = sphere.gameObject.AddComponent<PanoramicMediaSphereBehaviour>();
+            _panoramicMediaSphereBehaviour.FileRead = this;
+            _panoramicMediaSphereBehaviour.Index = index;
+            _panoramicMediaSphereBehaviour.ImagePath = mediaPath;
+            string ext = Path.GetExtension(mediaPath);
+
+            switch (ext)
+            {
+                case ".mp4":
+                case ".asf":
+                case ".avi":
+                case ".dv":
+                case ".m4v":
+                case ".mov":
+                case ".mpg":
+                case ".mpeg":
+                case ".ogv":
+                case ".vp8":
+                case ".webm":
+                case ".wmv":
+                    panoramicPoint.Type = PanoramicSphere.MediaType.Video;
+                    break;
+            }
+
+            if (panoramicPoint.Type == PanoramicSphere.MediaType.Image)
+            {
+                _panoramicMediaSphereBehaviour.Type = PanoramicSphere.MediaType.Image;
+                Shader skyboxShader = Shader.Find("Skybox/Panoramic");
+			    Material skyBox = new Material(skyboxShader);
+                skyBox.name = mediaName;
+                RenderSettings.skybox = skyBox;
+                _materials.Add(skyBox);
+                _panoramicMediaSphereBehaviour.PanoramicImage = skyBox;
+            }
+            else if(panoramicPoint.Type == PanoramicSphere.MediaType.Video)
+            {
+                _panoramicMediaSphereBehaviour.Type = PanoramicSphere.MediaType.Video;
+            }
+
+            _panoramicMediaSphereBehaviours.Add(_panoramicMediaSphereBehaviour);
+
+            index++;
+
+            _cameraRig.transform.position = panoramicPoint.Position;
+
+			if (panoramicPoint.FileName.Contains("erroneous"))
+			{
+				sphere.gameObject.SetActive(false);
+			}
+			else
+			{
+				Point thisPoint = new Point(mediaName, Path.GetFileNameWithoutExtension(mediaPath),panoramicPoint.Position, index);
+				_convertedPoints.Add(thisPoint);
+			}
+
+			if (firstImageFound && File.Exists(mediaPath))
+			{
+				firstImageFound = false;
+				firstCorrectPath = _panoramicMediaSphereBehaviour;
+			}
+		}
+
+        //Use the first successfully parsed piece of media as the inital entry point for the experience
+		if (firstCorrectPath != null)
+			firstCorrectPath.LoadMedia();
+
+	}
+    #endregion //Private Methods
+
+    #region Unity Methods
+    /// <summary>
+    /// Initialise all collections and establish the instance.
+    /// </summary>
+    private void Awake()
+	{
+        if (Instance == null)
+            Instance = this;
+        else
+            return;
+		_cameraRig = XRUserManager.Instance.CurrentXRUser;
+		_panoramicMediaSphereBehaviours = new List<PanoramicMediaSphereBehaviour>();
+        _panoramicPoints = new List<PanoramicSphere>();
+		_materials = new List<Material>();
+		_imageNames = new List<string>();
+	}
+
+    /// <summary>
+    /// Poll continously to determine if the Raycast is hitting a media sphere
+    /// </summary>
+	private void Update()
+	{
+		if (RayCastController.Instance.IsHit && RayCastController.Instance.HitInfo.collider != null)
+		{
+			if (RayCastController.Instance.HitInfo.collider.GetComponent<PanoramicMediaSphereBehaviour>() != null)
+			{
+                PanoramicMediaSphereBehaviour currentHoveredWaypoint = RayCastController.Instance.HitInfo.collider.GetComponent<PanoramicMediaSphereBehaviour>();
+				if (XRInputManager.Instance.PrimaryTriggerDown)
+					currentHoveredWaypoint.LoadMedia();
+
+			}
+		}
+	}
+
+    /// <summary>
+    /// On destroy clean up all Panoramic media Spheres
+    /// </summary>
+	private void OnDestroy()
+	{
+		if(ApplicationManager.Instance.IsInPanoramic)
+		{
+			foreach(PanoramicMediaSphereBehaviour behaviour in _panoramicMediaSphereBehaviours)
+			{
+				GameObject.Destroy(behaviour.gameObject);
+			}
+		}
+	}
+    #endregion //Unity Methods
+
+}
